5744a85e22f0996c47c429da9521b022a3189b08
sinyeongseok <sinyeongseok99@gmail.com>
2023-07-09 20:05:59 +0900

    feat: TransactionType, PaymentMethod, GenderCode enum 추가

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export enum TransactionType {
+  Expense = 'expense',
+  Income = 'income',
+}
+
+export enum PaymentMethod {
+  Cash = 'cash',
+  Card = 'card',
+}
+
+export enum GenderCode {
+  M = 'm',
+  F = 'f',
+}
